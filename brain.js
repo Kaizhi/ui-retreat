d47e491defc72aa45816ca45e5e04796c047b948
Cynthia Li <cynthia.li@centro-000206.centro.net>
2018-07-11 15:58:06 -0400

    bid units at 1/5 of current balance

--- brain.js
+++ brain.js
@@ -6,6 +6,7 @@ const pub = redis.createClient(6379, 'beat42'); //need 2 clients: can't publish 
 
 const REGISTRY_KEY = 911;
 let sig_key = undefined;
+let balance = undefined;
 
 sub.subscribe("exchange.market", (arg, channel) => console.log(`Subscribed to ${channel}.`));
 // subscribe to groot specific logs
@@ -13,6 +14,7 @@ sub.subscribe("exchange.logs.groot", (arg, channel) => console.log(`Subscribed t
 sub.subscribe(`exchange.registry.${REGISTRY_KEY}`);
 sub.subscribe('exchange.balances.groot');
 sub.subscribe("exchange.bids");
+sub.subscribe("exchange.balances.groot");
 
 sub.on("message", (topic, msg) => {
   console.log("\x1b[33m", topic + ' ==========================');
@@ -34,6 +36,10 @@ sub.on("message", (topic, msg) => {
     let response = JSON.parse(msg);
     sig_key = response.key;
     console.log("\x1b[0m", 'sig key set: ', sig_key);
+  } else if (topic === "exchange.balances.groot") {
+    let response = JSON.parse(msg);
+    balance = response.balance;
+    console.log("BALANCE", response);
   }
   else {
     console.log("\x1b[0m", msg);
@@ -51,10 +57,12 @@ handleMarketMessage = (market) => {
     return;
   }
 
+  let quantity = parseInt((balance/5) / (market.price + 1));
+
   pub.publish('exchange.bids', JSON.stringify({
     offerId: market.offerId,
     slot: 'groot',
-    qty: 1,
+    qty: quantity || 1,
     price: market.price + 1,
     signed: utils.getSignature(sig_key, market.offerId)
   }));
