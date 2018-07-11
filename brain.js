e884c1a5e2bdd30778ef98ca2fa427814062c316
Kaizhi Wei <Kaizhi.Wei@centro.net>
2018-07-11 13:03:20 -0400

    start bidding

--- brain.js
+++ brain.js
@@ -3,7 +3,9 @@ let utils = require('./utils.js');
 const redis = require('redis');
 const sub = redis.createClient(6379, 'beat42');
 const pub = redis.createClient(6379, 'beat42'); //need 2 clients: can't publish after subscribed
+
 const REGISTRY_KEY = 911;
+let sig_key = undefined;
 
 sub.subscribe("exchange.market", (arg, channel) => console.log(`Subscribed to ${channel}.`));
 // subscribe to groot specific logs
@@ -14,19 +16,22 @@ sub.on("message", (topic, msg) => {
   console.log(topic);
   if (topic === "exchange.market") {
     let market = JSON.parse(msg);
+    console.log(market);
     let update = {
         component: "groot",
         data: {path: 'message', value: `Units available: ${market.units} at \$${market.price}.`}
     };
     pub.publish("main.model", JSON.stringify(update));
+
+    handleMarketMessage(market);
   }
   else if (topic === "exchange.logs.groot") {
     console.log(msg);
   }
   else if (topic === `exchange.registry.${REGISTRY_KEY}`) {
     let response = JSON.parse(msg);
-    console.log(utils.getSignature(msg.key));
-
+    sig_key = response.key;
+    console.log('sig key set: ', sig_key);
   }
   else {
     console.log(topic + ':', msg);
@@ -37,6 +42,21 @@ const register = () => {
   pub.publish('exchange.registry', JSON.stringify( {slot:'groot', name:'Groot Marijuana Blockchain Inc.', reqId: REGISTRY_KEY }));
 }
 
+
+handleMarketMessage = (market) => {
+  if (!sig_key) {
+    console.log("Can't bid yet, haven't received a key");
+    return;
+  }
+
+  pub.publish('exchange.bids', JSON.stringify({
+    offerId: market.offerId,
+    slot: 'groot',
+    qty: 1,
+    price: market.price + 1,
+    signed: utils.getSignature(sig_key, market.offerId)
+  }));
+}
 register();
 
 
